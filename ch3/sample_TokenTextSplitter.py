7ef0a49c817cfb41a1d3a69804aba0d491435658
eric-epsilla <eric@epsilla.com>
2024-06-09 02:46:09 +0000

    update TokenTextSplitter
    Signed-off-by: eric-epsilla <eric@epsilla.com>

--- ch3/sample_TokenTextSplitter.py
+++ ch3/sample_TokenTextSplitter.py
@@ -1,3 +1,6 @@
+import os
+os.environ["OPENAI_API_KEY"] = "sk-****"
+
 from llama_index.core import Document
 from llama_index.core.node_parser import TokenTextSplitter
 
@@ -8,6 +11,8 @@ doc = Document(
     ),
     metadata={"author": "John Smith"}
 )  
+# Metadata length (6) is close to chunk size (12). Resulting chunks are less than 50 tokens. Consider increasing the chunk size or decreasing the size of your metadata to avoid this.
+
 splitter = TokenTextSplitter( 
     chunk_size=12, 
     chunk_overlap=0, 
