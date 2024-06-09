12c95cec75bdf605e995b9546bb72bf448a01e36
eric-epsilla <eric@epsilla.com>
2024-06-09 02:58:44 +0000

    update llama_index.readers.web to llama_index.legacy.readers.web
    Signed-off-by: eric-epsilla <eric@epsilla.com>

--- ch4/sample_reader_SimpleWebPageReader.py
+++ ch4/sample_reader_SimpleWebPageReader.py
@@ -1,4 +1,7 @@
-from llama_index.readers.web import SimpleWebPageReader
+from llama_index.legacy.readers.web import SimpleWebPageReader
+from llama_index.legacy.readers.web import BeautifulSoupWebReader
+from llama_index.legacy.readers.web import TrafilaturaWebReader
+
 
 urls = ["https://docs.llamaindex.ai"]
 documents = SimpleWebPageReader().load_data(urls)
